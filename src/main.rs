099630a63dd4c5b5152afb2f5e04c2bf70a05baa
Juho Röyskö <roysko.juho@gmail.com>
2023-03-04 19:18:29 +0200

    Small improvements to UI

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ mod state;
 async fn main() {
     let mut state = GameState::new(true);
 
+    let mut prev_bot_move = (-1, -1);
     loop {
         let tile_size = f32::min(screen_width() / 15.0, screen_height() / 15.0);
 
-        if let Some(_) = get_last_key_pressed() {
-            break;
+        if let Some(keypress) = get_last_key_pressed() {
+            if keypress == macroquad::input::KeyCode::Q {
+                break;
+            }
         }
 
         if state.player_turn {
@@ -28,6 +31,7 @@ async fn main() {
             }
         } else {
             let (x, y) = bot_move(&mut state);
+            prev_bot_move = (x as i32, y as i32);
 
             if x <= 14 && y <= 14 && state.board[x][y] == Tile::Empty {
                 state.board[x][y] = Tile::O;
@@ -61,6 +65,11 @@ async fn main() {
                 WHITE,
             );
         }
+        if state.player_turn && prev_bot_move.1 >= 0 {
+            let center_x = prev_bot_move.0 as f32 * tile_size + tile_size / 2.0;
+            let center_y = prev_bot_move.1 as f32 * tile_size + tile_size / 2.0;
+            draw_circle(center_x, center_y, tile_size / 2.0 - padding, RED);
+        }
 
         for x in 0..15 {
             for y in 0..15 {
@@ -88,7 +97,7 @@ async fn main() {
                 if state.board[x][y] == Tile::O {
                     let center_x = x as f32 * tile_size + tile_size / 2.0;
                     let center_y = y as f32 * tile_size + tile_size / 2.0;
-                    draw_circle(center_x, center_y, tile_size / 2.0 - padding, RED);
+                    draw_circle_lines(center_x, center_y, tile_size / 2.0 - padding, 1.0, RED);
                 }
             }
         }
